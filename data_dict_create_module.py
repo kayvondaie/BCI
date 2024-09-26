b6b6b9bb84ccc01a57d55e9f2e74810d7eaab658
kayvondaie <135642800+kayvondaie@users.noreply.github.com>
2024-09-26 08:45:43 -0700

    Add files via upload

--- data_dict_create_module.py
+++ data_dict_create_module.py
@@ -56,6 +56,37 @@ def main(folder):
         data['dist'], data['conditioned_neuron_coordinates'], data['conditioned_neuron'], data['cn_csv_index'] = find_conditioned_neurons(siHeader,stat)
         data['dt_si'] = 1/float(siHeader['metadata']['hRoiManager']['scanFrameRate'])
         
+        numtrl = data['F'].shape[2]
+        BCI_thresholds = np.full((2, numtrl), np.nan)
+        siHeader = np.load(folder + r'/suite2p_BCI/plane0/siHeader.npy', allow_pickle=True).tolist()
+
+        # Determine the base for file names
+        if isinstance(siHeader['siBase'], str):
+            base = siHeader['siBase']
+        else:
+            base = siHeader['siBase'][0]
+
+        # Iterate over trials and attempt to load the corresponding threshold files
+        for i in range(numtrl):
+            try:
+                st = folder + base + r'_threshold_' + str(i+1) + r'.mat'
+                
+                # Check if the file exists before trying to load it
+                if os.path.exists(st):
+                    threshold_data = scipy.io.loadmat(st)
+                    BCI_thresholds[:, i] = threshold_data['BCI_threshold'].flatten()
+                    
+            except:
+                pass  # Ignore any exceptions and continue with the next iteration
+        data['BCI_thresholds'] = BCI_thresholds
+        
+        csv_files = glob.glob(os.path.join(folder, base+'_IntegrationRois' + '_*.csv'))
+        csv_files = sorted(csv_files, key=lambda x: int(x.split('_')[-1].split('.')[0]))
+        csv_data = []
+        for i in range(len(csv_files)):
+            csv_file = csv_files[i]
+            csv_data.append(pd.read_csv(csv_file))        
+        data['roi_csv'] = np.concatenate(csv_data)
     
     # photostim data
     if os.path.isdir(folder +r'/suite2p_photostim/'):
@@ -110,37 +141,7 @@ def main(folder):
  
     
 
-    numtrl = data['F'].shape[2]
-    BCI_thresholds = np.full((2, numtrl), np.nan)
-    siHeader = np.load(folder + r'/suite2p_BCI/plane0/siHeader.npy', allow_pickle=True).tolist()
-
-    # Determine the base for file names
-    if isinstance(siHeader['siBase'], str):
-        base = siHeader['siBase']
-    else:
-        base = siHeader['siBase'][0]
-
-    # Iterate over trials and attempt to load the corresponding threshold files
-    for i in range(numtrl):
-        try:
-            st = folder + base + r'_threshold_' + str(i+1) + r'.mat'
-            
-            # Check if the file exists before trying to load it
-            if os.path.exists(st):
-                threshold_data = scipy.io.loadmat(st)
-                BCI_thresholds[:, i] = threshold_data['BCI_threshold'].flatten()
-                
-        except:
-            pass  # Ignore any exceptions and continue with the next iteration
-    data['BCI_thresholds'] = BCI_thresholds
     
-    csv_files = glob.glob(os.path.join(folder, base+'_IntegrationRois' + '_*.csv'))
-    csv_files = sorted(csv_files, key=lambda x: int(x.split('_')[-1].split('.')[0]))
-    csv_data = []
-    for i in range(len(csv_files)):
-        csv_file = csv_files[i]
-        csv_data.append(pd.read_csv(csv_file))        
-    data['roi_csv'] = np.concatenate(csv_data)
 
     np.save(folder + r'data_'+data['mouse']+r'_'+data['session']+r'.npy',data)
     #np.save(folder + r'data_'+data['mouse']+r'_'+data['session']+r'_'+str(int(np.round(np.random.rand()*100000)))+r'.npy',data)
