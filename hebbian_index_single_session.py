e09768184c8861cc812209e892ee6fb806d5adf4
kayvondaie <135642800+kayvondaie@users.noreply.github.com>
2025-01-15 08:50:26 -0800

    Update hebbian_index_single_session.py

--- hebbian_index_single_session.py
+++ hebbian_index_single_session.py
@@ -4,14 +4,74 @@ Created on Tue Jan 14 09:11:41 2025
 
 @author: kayvon.daie
 """
-
 #%%
+direct_cells = []
+indirect_cells = []
+AMP = []
+for epoch_i in range(2):
+    if epoch_i == 0:
+        stimDist = data['photostim']['stimDist']
+        favg_raw = data['photostim']['favg_raw']
+    else:
+        stimDist = data['photostim2']['stimDist']
+        favg_raw = data['photostim2']['favg_raw']
+    favg = favg_raw*0
+    for i in range(favg.shape[1]):
+        favg[:, i] = (favg_raw[:, i] - np.nanmean(favg_raw[0:3, i]))/np.nanmean(favg_raw[0:3, i])
+    favg[18:23, :, :] = np.nan
+    
+    favg = np.apply_along_axis(
+    lambda m: np.interp(
+        np.arange(len(m)),
+        np.where(~np.isnan(m))[0] if np.where(~np.isnan(m))[0].size > 0 else [0],  # Ensure sample points are non-empty
+        m[~np.isnan(m)] if np.where(~np.isnan(m))[0].size > 0 else [0]             # Use a fallback value if empty
+    ),
+    axis=0,
+    arr=favg
+    )
+
+    
+    
+    amp = np.nanmean(favg[25:35, :, :], axis=0) - np.nanmean(favg[10:19, :, :], axis=0)
+    AMP.append(amp)
+    N = favg.shape[2]
+    N = 10
+    offset = 0
+    # Create a figure and axes for a 5x5 grid
+    fig, axes = plt.subplots(N,N, figsize=(10, 10))
+    stim_cells = np.argmin(stimDist, axis=0)
+    # Loop through each row and column to customize subplots
+    direct = []
+    indirect = []
+    for gi in range(N):
+        for ci in range(N):
+            # Access the subplot at [i, j]
+            ax = axes[gi, ci]
+            if ci == gi:
+                col = 'r'
+                direct.append(favg[10:40,stim_cells[ci+offset],gi+offset])
+            else:
+                col = 'k'
+                indirect.append(favg[10:40,stim_cells[ci+offset],gi+offset])
+            ax.plot(favg[10:40,stim_cells[ci+offset],gi+offset],color = col)
+            if ci != gi:
+                ax.set_ylim(-.1,.1)
+            ax.set_xticks([])
+            ax.set_yticks([])
+    plt.tight_layout()
+    plt.show()
+    direct = np.asarray(direct)
+    indirect = np.asarray(indirect)
+    direct_cells.append(np.nanmean(direct,axis=0))
+    indirect_cells.append(np.nanmean(indirect,axis=0))
+#%%
+stimDist = data['photostim']['stimDist']
 plt.figure(figsize=(8,4))  # Set figure size to 10x10 inches
 df = data['df_closedloop']
 cc = np.corrcoef(df)
 F = data['F']
 ko = np.nanmean(F[120:200,:,0:10],axis=0)
-k = np.nanmean(F[120:200,:,0:],axis=0)
+k = np.nanmean(F[120:240,:,0:],axis=0)
 cc = np.corrcoef(k)
 cco = np.corrcoef(ko)
 ei = 1;
@@ -51,4 +111,8 @@ pf.mean_bin_plot(X,Y-Yo,5,1,1,'k')
 plt.xlabel('Pre-post correlation')
 plt.ylabel('$\Delta W_{i,j}$')
 plt.tight_layout()
+plt.title(data['mouse'] + ' ' + data['session'])
+#%%
+
+
 
